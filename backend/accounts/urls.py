6486878a88cabf0e05e8f7e168395020a4010e46
Jonathan Wu <Jtzw21@gmail.com>
2026-06-07 23:43:05 -0400

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("auth/", include("rest_framework.urls")),  # For login/logout
     path("register/", register_user, name="register"),  # For user registration
     path("me/", get_user, name="me"),  # For getting user info
-    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"), # For obtaining JWT token
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"), # For refreshing JWT token
 ]
